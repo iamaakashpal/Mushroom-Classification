359de2cc32a383bdc775dbb5958b9f9dfc37d5a4
iamaakashpal <aakashpal1198@gmail.com>
2023-11-09 01:30:00 +0530

    setup.py file updated

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from mushroom.logger import logging
 
 app = Flask(__name__)
 
+logging.info("Log file testing")
 @app.route('/',methods=["GET","POST"])
+def home():
+    logging.info("Successfully Tested.")
+    return "Home Page"
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
--- setup.py
+++ setup.py
@@ -1,1 +1,42 @@
+from setuptools import setup,find_packages
+from typing import List
 
+PROJECT_NAME = "Mushroom Classification"
+
+VERSION = "0.0.1"
+
+AUTHOR = "Aakash Pal"
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+
+
+def get_requirements_list() -> List[str]:
+
+    """
+    Description : This function is going to return the list of requirements mentioned in the requirements.txt file.
+    Input : file_name
+    Output : List of requirements.txt i.e. ['pandas','numpy',...'seaborn']
+    Name : Aakash Pal
+    Date : 09-Nov-2023    
+    """
+
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+
+        requirement_list = requirement_file.readlines()
+        
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+setup(
+    name = PROJECT_NAME,
+    version = VERSION,
+    author = AUTHOR,
+    packages = find_packages(), 
+    install_requires = get_requirements_list()
+)
